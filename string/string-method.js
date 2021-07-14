438529f5b59900357795251c1be007e4f0fd1564
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-07-15 01:16:02 +0900

    string method

---
+++ string/string-method.js
@@ -0,0 +1,7 @@
+// ES2015 string의 새로운 메소드들
+
+let str = "hello world~ ^.^";
+let matchstr = "^.^";
+console.log("start:", str.startsWith(matchstr));
+console.log("end:", str.endsWith(matchstr));
+console.log("include:", str.includes(matchstr));
